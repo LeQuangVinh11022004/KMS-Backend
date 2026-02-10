9f34e12096eaa2672f45ad816a02dae3f0184674
ChesterK01 <tuandnse173227@fpt.edu.vn>
2026-02-10 07:38:28 +0700

    add crud schoolyears api

---
+++ KMS.Service/Interfaces/ISchoolYearService.cs
@@ -0,0 +1,17 @@
+﻿using KMS.Service.DTOs.SchoolYear;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMS.Service.Interfaces
+{
+    public interface ISchoolYearService
+    {
+        Task<List<SchoolYearDTO>> GetAllAsync();
+        Task<SchoolYearDTO?> GetByIdAsync(int id);
+        Task CreateAsync(SchoolYearCreateDTO dto, int createdBy);
+        Task<bool> UpdateAsync(int id, SchoolYearUpdateDTO dto, int updatedBy);
+    }
+}
